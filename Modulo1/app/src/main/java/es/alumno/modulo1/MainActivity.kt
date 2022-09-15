cf034a5953e73d1e88d643e19e115fd3cbe1d0f1
jmcompanyg <jmcompany@igformacion.com>
2022-09-15 12:21:04 +0200

    Update MainActivity.kt
    Estados de una actividad

--- Modulo1/app/src/main/java/es/alumno/modulo1/MainActivity.kt
+++ Modulo1/app/src/main/java/es/alumno/modulo1/MainActivity.kt
@@ -2,12 +2,43 @@ package es.alumno.modulo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        //CAMBIO
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
     }
 }
