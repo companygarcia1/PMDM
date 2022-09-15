6f68f260ed61e0600d2774e88a984a199b7502fe
jmcompanyg <jmcompany@igformacion.com>
2022-09-15 12:37:16 +0200

    Dynamic Title

--- Modulo1/app/src/main/java/es/alumno/modulo1/MainActivity.kt
+++ Modulo1/app/src/main/java/es/alumno/modulo1/MainActivity.kt
@@ -3,13 +3,26 @@ package es.alumno.modulo1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
 
+    lateinit var txtTitle: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViews()
+        initializeViews()
+    }
+
+    private fun findViews(){
+        txtTitle = findViewById(R.id.txtTitle)
+    }
+
+    private fun initializeViews(){
+        txtTitle.setText("Jose Maria Compañy Garcia")
     }
 
     override fun onStart() {
